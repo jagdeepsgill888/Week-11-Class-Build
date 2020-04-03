3d6f4f38af370435aabd8d60c847a3fbd402a6fd
jagdeepsgill888 <59809683+jagdeepsgill888@users.noreply.github.com>
2020-04-02 20:27:31 -0400

    updating js file

--- js/app.js
+++ js/app.js
@@ -22,10 +22,61 @@ new Chart(document.getElementById("lineChart"), {
   data: {
     labels: ["January", "February", "March"],
     datasets: [
+
       {
         label: "China",
-        data: ["1179", "79824", "811171"]
-      }
+        data: ["1179", "79824", "81171"],
+        fill: false,
+        borderColor: '#3e95cd',
+      },
+
+      {
+        label: 'United Sates ',
+        data: [0, 60, 42886],
+        fill: false,
+        borderColor: '#3cba9f',
+      },
+
+      {
+       label: 'Canada',
+       data: [0, 20, 2091],
+       borderColor: '#c45850',
+       fill: false, 
+      },
+
+
     ]
-  }
+  },
+    
+  options: {
+      title: {
+        display: true,
+        text: 'Coronavrius infection amounts'
+      }
+    }
+
 });
+
+
+new Chart(document.getElementById("pieChart"), {
+    type: 'pie',
+    data: {
+      labels: ['Apple', 'Cherry', 'Pumpkin'],
+      datasets: [
+           {
+             data: [14362, 97544, 500],
+             backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f']
+           }
+
+      ]
+    },
+
+     options: {
+      title: {
+        display: true,
+        text: 'Peoples preferred pic'
+      }
+    }        
+
+});
+
